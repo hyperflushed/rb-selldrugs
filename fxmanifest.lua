6c532015f7be62df666fa2e1acbb4657a6ee6abd
hyperflushed <80010634+hyperflushed@users.noreply.github.com>
2021-06-02 01:12:56 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+
+game "gta5"
+author "merty - megvalatzson"
+
+client_scripts {
+    "client.lua",
+    "config.lua"
+}
+
+server_scripts {
+    "server.lua",
+    "config.lua"
+}
